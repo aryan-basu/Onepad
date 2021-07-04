1361b261cc03d76134cc3a0c007690becb6a4786
Navee Ramesh <naveenarameshvardhini@gmail.com>
2021-07-04 19:08:19 +0530

    constants setup

--- onepad/lib/Helpers/colorhelper.dart
+++ onepad/lib/Helpers/colorhelper.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 final Color background = Colors.white;
-final Color darkcolor = Colors.teal;
-final Color lightcolor = Colors.teal.withOpacity(0.5);
+final Color darkcolor = Colors.purple;
+final Color lightcolor = Colors.purple.withOpacity(0.5);
 final Color darktextcolor = Colors.black;
 final Color lighttextcolor = Colors.white;
-final Color commoncolor = Colors.teal;
+final Color commoncolor = Colors.purple;
 final int minradius = 10;
 final int maxradius = 40;
 const int screenpadding = 15;
