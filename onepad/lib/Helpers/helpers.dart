190a388a603ec4f82390d86d29af09c895d9fbe8
Navee Ramesh <naveenarameshvardhini@gmail.com>
2021-07-04 19:05:46 +0530

    constants setup

---
+++ onepad/lib/Helpers/helpers.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:onepad/Helpers/colorhelper.dart';
+
+
+class Helper {
+  int radius = 10;
+  int maxradius = 40;
+  static Widget text(String msg, int size, int spacing, Color color) {
+    return Text(
+      msg,
+      textAlign: TextAlign.center,
+      style: GoogleFonts.ubuntu(
+          fontWeight: FontWeight.bold,
+          fontSize: size.toDouble(),
+          color: color,
+          letterSpacing: spacing.toDouble()),
+    );
+  }
+
+  static Widget subtext(String msg, int size, int spacing, Color color) {
+    return Text(
+      msg,
+      textAlign: TextAlign.center,
+      style: GoogleFonts.ubuntu(
+          fontWeight: FontWeight.normal,
+          fontSize: size.toDouble(),
+          color: color,
+          letterSpacing: spacing.toDouble()),
+    );
+  }
+
+  static Widget button(String msg, int radius) {
+    return Container(
+      child: Center(
+        child: Helper.text(msg, 18, 0, lighttextcolor),
+      ),
+      height: 50,
+      width: 150,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(radius.toDouble()),
+          gradient: LinearGradient(
+              colors: [Colors.teal, Colors.teal.withOpacity(0.5)])),
+    );
+  }
+}
---
+++ onepad/lib/Screens/SingupScreen/SignupScreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({Key? key}) : super(key: key);
+
+  @override
+  _SignUpScreenState createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
