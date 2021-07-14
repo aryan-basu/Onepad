27fee5702cfed51b367c5e68f62d90edf6adb53a
Navee Ramesh <naveenarameshvardhini@gmail.com>
2021-07-14 19:27:47 +0530

    Facebook Signin

--- lib/Screens/SignInScreen/SignInScreen.dart
+++ lib/Screens/SignInScreen/SignInScreen.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+// import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:onepad/Helpers/colorhelper.dart';
 import 'package:onepad/Helpers/helpers.dart';
@@ -27,8 +28,46 @@ class _SignInScreenState extends State<SignInScreen> {
 
   bool isloading = false;
   bool passwordvisibility = false;
+  // static final FacebookLogin facebookSignIn = new FacebookLogin();
 
+  // String _message = 'Log in/out by pressing the buttons below.';
   final formKey = GlobalKey<FormState>();
+  // Future<Null> _login() async {
+  //   final FacebookLoginResult result = await facebookSignIn.logIn(['email']);
+
+  //   switch (result.status) {
+  //     case FacebookLoginStatus.loggedIn:
+  //       final FacebookAccessToken accessToken = result.accessToken;
+  //       _showMessage('''
+  //        Logged in!
+
+  //        Token: ${accessToken.token}
+  //        User id: ${accessToken.userId}
+  //        Expires: ${accessToken.expires}
+  //        Permissions: ${accessToken.permissions}
+  //        Declined permissions: ${accessToken.declinedPermissions}
+  //        ''');
+  //       break;
+  //     case FacebookLoginStatus.cancelledByUser:
+  //       _showMessage('Login cancelled by the user.');
+  //       break;
+  //     case FacebookLoginStatus.error:
+  //       _showMessage('Something went wrong with the login process.\n'
+  //           'Here\'s the error Facebook gave us: ${result.errorMessage}');
+  //       break;
+  //   }
+  // }
+
+  // Future<Null> _logOut() async {
+  //   await facebookSignIn.logOut();
+  //   _showMessage('Logged out.');
+  // }
+
+  // void _showMessage(String message) {
+  //   setState(() {
+  //     _message = message;
+  //   });
+  // }
 
   Future<void> _signin() async {
     if (formKey.currentState.validate()) {
@@ -101,181 +140,149 @@ class _SignInScreenState extends State<SignInScreen> {
                   padding: const EdgeInsets.all(40.0),
                   child: Form(
                     key: formKey,
-                    child: Column(
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(
-                              color: lightcolor.withOpacity(0.2),
-                              borderRadius: BorderRadius.circular(40)),
-                          child: TextFormField(
-                            validator: (val) {
-                              return RegExp(
-                                          r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                                      .hasMatch(val)
-                                  ? null
-                                  : "Please provide a valid email";
-                            },
-                            controller: emailcontroller,
-                            style: GoogleFonts.ubuntu(color: darktextcolor),
-                            decoration: InputDecoration(
-                                prefixIcon: Icon(
-                                  Icons.email,
-                                  color: darkcolor,
-                                  size: 15,
-                                ),
-                                hintText: 'Email',
-                                hintStyle:
-                                    GoogleFonts.ubuntu(color: darktextcolor),
-                                enabledBorder: OutlineInputBorder(
-                                    borderSide:
-                                        BorderSide(color: Colors.transparent)),
-                                focusedBorder: OutlineInputBorder(
-                                    borderSide:
-                                        BorderSide(color: Colors.transparent))),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Container(
-                          decoration: BoxDecoration(
-                              color: lightcolor.withOpacity(0.2),
-                              borderRadius: BorderRadius.circular(40)),
-                          child: TextFormField(
-                            controller: passwordcontroller,
-                            validator: (val) {
-                              return val.length < 4
-                                  ? 'Provide a strong password'
-                                  : null;
-                            },
-                            decoration: InputDecoration(
-                                prefixIcon: Icon(
-                                  Icons.lock,
-                                  size: 15,
-                                  color: darkcolor,
-                                ),
-                                hintText: 'Password',
-                                hintStyle:
-                                    GoogleFonts.ubuntu(color: darktextcolor),
-                                enabledBorder: OutlineInputBorder(
-                                    borderSide:
-                                        BorderSide(color: Colors.transparent)),
-                                focusedBorder: OutlineInputBorder(
-                                    borderSide:
-                                        BorderSide(color: Colors.transparent)),
-                                suffixIcon: IconButton(
-                                  onPressed: () {
-                                    setState(() {
-                                      passwordvisibility = !passwordvisibility;
-                                    });
-                                  },
-                                  icon: passwordvisibility
-                                      ? Icon(Icons.visibility)
-                                      : Icon(Icons.visibility_off),
-                                  color: darkcolor,
-                                  iconSize: 15,
-                                )),
-                            obscureText: !passwordvisibility,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            _signin();
+                    child: Column(children: [
+                      Container(
+                        decoration: BoxDecoration(
+                            color: lightcolor.withOpacity(0.2),
+                            borderRadius: BorderRadius.circular(40)),
+                        child: TextFormField(
+                          validator: (val) {
+                            return RegExp(
+                                        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                    .hasMatch(val)
+                                ? null
+                                : "Please provide a valid email";
                           },
-                          child: Container(
-                            height: 60,
-                            decoration: BoxDecoration(
+                          controller: emailcontroller,
+                          style: GoogleFonts.ubuntu(color: darktextcolor),
+                          decoration: InputDecoration(
+                              prefixIcon: Icon(
+                                Icons.email,
                                 color: darkcolor,
-                                borderRadius: BorderRadius.circular(40)),
-                            child: Center(
-                                child: Helper.text(
-                                    'SignIn', 20, 1, lighttextcolor)),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Row(children: <Widget>[
-                          Expanded(
-                            child: new Container(
-                                margin: const EdgeInsets.only(
-                                    left: 10.0, right: 20.0),
-                                child: Divider(
-                                  color: darkcolor,
-                                  height: 20,
-                                )),
-                          ),
-                          Helper.text('OR', 16, 1, darktextcolor),
-                          Expanded(
-                            child: new Container(
-                                margin: const EdgeInsets.only(
-                                    left: 20.0, right: 10.0),
-                                child: Divider(
-                                  color: darkcolor,
-                                  height: 20,
-                                )),
-                          ),
-                        ]),
-                        GestureDetector(
-                          onTap: () async {
-                            await authentication
-                                .googleSignIn()
-                                .whenComplete(() => {
-                                      Navigator.pushReplacement(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (b) => HomeScreen()))
-                                    });
-                          },
-                          child: Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: Container(
-                              height: 60,
-                              decoration: BoxDecoration(
-                                  color: darkcolor,
-                                  borderRadius: BorderRadius.circular(40)),
-                              child: Padding(
-                                padding: const EdgeInsets.all(.0),
-                                child: Row(
-                                  // mainAxisAlignment:
-                                  //     MainAxisAlignment.spaceEvenly,
-                                  children: [
-                                    SizedBox(
-                                      width: 40,
-                                    ),
-                                    Container(
-                                        height: 20,
-                                        child: Image.network(
-                                            'https://image.flaticon.com/icons/png/128/2702/2702602.png')),
-                                    SizedBox(
-                                      width: 30,
-                                    ),
-                                    Helper.text('SignIn with Google', 18, 1,
-                                        lighttextcolor),
-                                  ],
-                                ),
+                                size: 15,
                               ),
+                              hintText: 'Email',
+                              hintStyle:
+                                  GoogleFonts.ubuntu(color: darktextcolor),
+                              enabledBorder: OutlineInputBorder(
+                                  borderSide:
+                                      BorderSide(color: Colors.transparent)),
+                              focusedBorder: OutlineInputBorder(
+                                  borderSide:
+                                      BorderSide(color: Colors.transparent))),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: lightcolor.withOpacity(0.2),
+                            borderRadius: BorderRadius.circular(40)),
+                        child: TextFormField(
+                          controller: passwordcontroller,
+                          validator: (val) {
+                            return val.length < 4
+                                ? 'Provide a strong password'
+                                : null;
+                          },
+                          decoration: InputDecoration(
+                              prefixIcon: Icon(
+                                Icons.lock,
+                                size: 15,
+                                color: darkcolor,
+                              ),
+                              hintText: 'Password',
+                              hintStyle:
+                                  GoogleFonts.ubuntu(color: darktextcolor),
+                              enabledBorder: OutlineInputBorder(
+                                  borderSide:
+                                      BorderSide(color: Colors.transparent)),
+                              focusedBorder: OutlineInputBorder(
+                                  borderSide:
+                                      BorderSide(color: Colors.transparent)),
+                              suffixIcon: IconButton(
+                                onPressed: () {
+                                  setState(() {
+                                    passwordvisibility = !passwordvisibility;
+                                  });
+                                },
+                                icon: passwordvisibility
+                                    ? Icon(Icons.visibility)
+                                    : Icon(Icons.visibility_off),
+                                color: darkcolor,
+                                iconSize: 15,
+                              )),
+                          obscureText: !passwordvisibility,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          _signin();
+                        },
+                        child: Container(
+                          height: 60,
+                          decoration: BoxDecoration(
+                              color: darkcolor,
+                              borderRadius: BorderRadius.circular(40)),
+                          child: Center(
+                              child:
+                                  Helper.text('SignIn', 20, 1, lighttextcolor)),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          GestureDetector(
+                            onTap: () async {
+                              await authentication
+                                  .googleSignIn()
+                                  .whenComplete(() => {
+                                        Navigator.pushReplacement(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (b) => HomeScreen()))
+                                      });
+                            },
+                            child: Container(
+                                height: 20,
+                                child: Image.network(
+                                    'https://image.flaticon.com/icons/png/128/2702/2702602.png')),
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              // _login();
+                            },
+                            child: Image.network(
+                              "https://image.flaticon.com/icons/png/128/733/733547.png",
+                              height: 20,
                             ),
                           ),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (b) => SignUpScreen()));
-                          },
-                          child: Helper.text('Do not have an account? Signup',
-                              10, 1, darkcolor),
-                        ),
-                      ],
-                    ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (b) => SignUpScreen()));
+                        },
+                        child:
+                            Helper.text('New User ? Signup', 10, 1, darkcolor),
+                      ),
+                    ]),
                   ),
                 ),
               ],
