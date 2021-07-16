d018bc27c1bef6ca8c222ac5360fd701e43c08d1
Navee Ramesh <naveenarameshvardhini@gmail.com>
2021-07-16 11:54:02 +0530

    Onboarding Screen

--- lib/Screens/Onboarding/Onboarding.dart
+++ lib/Screens/Onboarding/Onboarding.dart
@@ -58,13 +58,15 @@ class _OnboardScreenState extends State<OnboardScreen> {
         child: Column(
           children: [
             Expanded(
-              flex: 3,
               child: PageView.builder(
                 controller: _controller,
                 itemCount: splashData.length,
                 itemBuilder: (BuildContext context, int index) {
                   return Column(children: [
-                    Spacer(flex: 2),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height / 8,
+                    ),
+                    // Spacer(flex: 1),
                     Padding(
                       padding: EdgeInsets.only(bottom: 10),
                       child: Helper.text("${(splashData[index]['title'])}", 20,
@@ -79,13 +81,19 @@ class _OnboardScreenState extends State<OnboardScreen> {
                             18, 0, Colors.grey),
                       ),
                     ),
-                    Spacer(flex: 2),
-                    AspectRatio(
-                      aspectRatio: 16 / 9,
-                      child: Image.asset('${(splashData[index]['image'])}',
-                          fit: BoxFit.contain),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height / 12,
                     ),
-                    Spacer(),
+                    Container(
+                        height: MediaQuery.of(context).size.height / 3,
+                        child: Image.asset('${(splashData[index]['image'])}')),
+                    // Spacer(flex: 1),
+                    // AspectRatio(
+                    //   aspectRatio: 16 / 9,
+                    //   child: Image.asset('${(splashData[index]['image'])}',
+                    //       fit: BoxFit.contain),
+                    // ),
+                    // Spacer(),
                   ]);
                 },
                 onPageChanged: (value) => setState(() {
@@ -93,110 +101,94 @@ class _OnboardScreenState extends State<OnboardScreen> {
                 }),
               ),
             ),
-            Expanded(
-                flex: 1,
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Padding(
-                          padding: EdgeInsets.only(top: 100),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: List.generate(splashData.length,
-                                (int index) => _buildDots(index: index)),
-                          )),
-                      Spacer(),
-                      SizedBox(
-                          height: 70,
-                          child: _currentPage != 2
-                              ? Padding(
-                                  padding: const EdgeInsets.only(left: 20.0),
-                                  child: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Padding(
-                                        padding:
-                                            const EdgeInsets.only(bottom: 20.0),
-                                        child: GestureDetector(
-                                            onTap: () {
-                                              Navigator.pushReplacement(
-                                                  context,
-                                                  MaterialPageRoute(
-                                                      builder: (b) =>
-                                                          SignUpScreen()));
-                                            },
-                                            child: Container(
-                                              child: Center(
-                                                child: Helper.subtext('Skip',
-                                                    14, 0, lighttextcolor),
-                                              ),
-                                              height: 40,
-                                              width: 100,
-                                              decoration: BoxDecoration(
-                                                  borderRadius:
-                                                      BorderRadius.circular(40),
-                                                  color: darkcolor),
-                                            )),
-                                      ),
-                                      Padding(
-                                        padding: const EdgeInsets.only(
-                                            right: 30, bottom: 20.0),
-                                        child: GestureDetector(
-                                            onTap: () {
-                                              if (_currentPage == 2) {
-                                                Navigator.pushReplacement(
-                                                    context,
-                                                    MaterialPageRoute(
-                                                        builder: (b) =>
-                                                            SignUpScreen()));
-                                              }
-                                              _controller
-                                                  .jumpToPage(++_currentPage);
-                                            },
-                                            child: Container(
-                                              child: Center(
-                                                child: Helper.subtext(
-                                                    'Continue',
-                                                    14,
-                                                    0,
-                                                    darkcolor),
-                                              ),
-                                              height: 50,
-                                              width: 100,
-                                              decoration: BoxDecoration(
-                                                  borderRadius:
-                                                      BorderRadius.circular(20),
-                                                  color: Colors.transparent),
-                                            )),
-                                      ),
-                                    ],
-                                  ),
-                                )
-                              : Padding(
-                                  padding: const EdgeInsets.only(bottom: 20.0),
-                                  child: GestureDetector(
-                                      onTap: () {
-                                        Navigator.pushReplacement(
-                                            context,
-                                            MaterialPageRoute(
-                                                builder: (b) =>
-                                                    SignUpScreen()));
-                                      },
-                                      child: Container(
-                                        child: Center(
-                                          child: Helper.subtext('Get Started',
-                                              14, 0, lighttextcolor),
-                                        ),
-                                        height: 50,
-                                        width: 300,
-                                        decoration: BoxDecoration(
-                                            borderRadius:
-                                                BorderRadius.circular(40),
-                                            color: darkcolor),
-                                      )),
-                                ))
-                    ])),
+            Padding(
+                padding: EdgeInsets.only(top: 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: List.generate(splashData.length,
+                      (int index) => _buildDots(index: index)),
+                )),
+            SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+                height: 70,
+                child: _currentPage != 2
+                    ? Padding(
+                        padding: const EdgeInsets.only(left: 20.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 20.0),
+                              child: GestureDetector(
+                                  onTap: () {
+                                    Navigator.pushReplacement(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (b) => SignUpScreen()));
+                                  },
+                                  child: Container(
+                                    child: Center(
+                                      child: Helper.subtext(
+                                          'Skip', 14, 0, lighttextcolor),
+                                    ),
+                                    height: 40,
+                                    width: 100,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(40),
+                                        color: darkcolor),
+                                  )),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  right: 30, bottom: 20.0),
+                              child: GestureDetector(
+                                  onTap: () {
+                                    if (_currentPage == 2) {
+                                      Navigator.pushReplacement(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (b) => SignUpScreen()));
+                                    }
+                                    _controller.jumpToPage(++_currentPage);
+                                  },
+                                  child: Container(
+                                    child: Center(
+                                      child: Helper.subtext(
+                                          'Continue', 14, 0, darkcolor),
+                                    ),
+                                    height: 50,
+                                    width: 100,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(20),
+                                        color: Colors.transparent),
+                                  )),
+                            ),
+                          ],
+                        ),
+                      )
+                    : Padding(
+                        padding: const EdgeInsets.only(bottom: 20.0),
+                        child: GestureDetector(
+                            onTap: () {
+                              Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (b) => SignUpScreen()));
+                            },
+                            child: Container(
+                              child: Center(
+                                child: Helper.subtext(
+                                    'Get Started', 14, 0, lighttextcolor),
+                              ),
+                              height: 50,
+                              width: 300,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(40),
+                                  color: darkcolor),
+                            )),
+                      )),
           ],
         ),
       ),
